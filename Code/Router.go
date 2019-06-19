da784163dd61c8b3c1a646fa10496815776979ec
Cordes-RJ <47733645+Cordes-RJ@users.noreply.github.com>
2019-06-19 08:35:19 -0400

    Remove Router
    Due to changes in reader and the removal of "parser sets", router is no longer necessary

--- Code/Router.go
+++
@@ -1,7 +0,0 @@
-package FdfExtract
-
-// Router must be able to identify the fdf format and pair it to an appropriate
-// parse function
-type Router interface {
-	PickReader([]byte, map[string]Parser) Parser
-}
