054cd3ee2d340775a346152559d584b9748d89ed
Cordes-RJ <richardj.cordes@gmail.com>
2019-06-20 13:47:02 -0400

    Reader modified, read as only export

--- Code/Reader.go
+++ Code/Reader.go
@@ -1,7 +1,8 @@
 package FdfExtract
 
-// FdfReader takes a filepath to an FDF document and outputs a CommentBlock
-type FdfReader interface {
-	ReadFdf(string) CommentBlock // takes filepath and returns CommentBlock
-	SetConcurrency(bool)         //set FdfReader to be Concurrent
+// A Reader takes a filepath to an FDF document and outputs a CommentBlock
+type Reader interface {
+	Read(string) (CommentBlock, error) // takes filepath and returns CommentBlock
+	enableConcurrency()                //set FdfReader to be Concurrent
+	disableConcurrency()               //set FdfReader to be not be Concurrent
 }
